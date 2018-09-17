ca23efba0cbae8f7e4607f0f02b07d6ecee0f0b1
Kim In Seop <opnayn@gmail.com>
2018-09-17 22:04:14 +0900

    Basic: Add ReadLine. - Read only one line and print it.
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

---
+++ Basic/ReadLine.kt
@@ -0,0 +1,11 @@
+/*
+ * 한줄 읽기
+ */
+
+fun main(args: Array<String>) {
+    // Java의 System.in 이라는 InputStream을 사용해 한줄을 읽어오는 함수입니다.
+    val line = readLine()!!
+
+    // Java의 System.out.println과 동일합니다.
+    println(line)
+}
