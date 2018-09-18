e2d7be7d8563111041e5fff27db84bb9dacb0ff4
Kim In Seop <opnayn@gmail.com>
2018-09-18 20:14:56 +0900

    Basic: Add HelloWorld
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

---
+++ Basic/HelloWorld.kt
@@ -0,0 +1,12 @@
+/*
+ * Hello, World!
+ *
+ * 모든 언어의 기초인 Hello, World! 출력 예제입니다.
+ */
+
+// Kotlin의 메인 함수입니다.
+// 컴파일 후 실행하게되면, 이 함수부터 실행하게 됩니다.
+// Kotlin 1.3이전 버전은 `args: Array<String>`을 생략할 수 없습니다.
+fun main(args: Array<String>) {
+    println("Hello, World!")
+}
