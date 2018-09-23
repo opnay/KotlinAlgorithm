736df725440fdfbbc9c05881d52b082c7a1ea833
Kim In Seop <opnayn@gmail.com>
2018-09-23 14:02:28 +0900

    BaekJoon/1: Add 4.Register
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

---
+++ BaekJoon/1. IO/4.Register.kt
@@ -0,0 +1,13 @@
+/*
+ * Register
+ *
+ * https://www.acmicpc.net/problem/7287
+ */
+
+// 각 항목은 본인의 아이디로 로그인한 후 우측상단의 아이디를 클릭하면 보실수 있습니다.
+fun main(args: Array<String>) {
+    // 본인의 백준온라인저지의 맞은 문제수를 적으면 됩니다.
+    println(3)
+    // 본인의 백준온라인저지의 아이디를 적으면됩니다.
+    println("opnayn")
+}
