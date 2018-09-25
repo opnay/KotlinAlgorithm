b2acc020ace5040ce877906790b4c4b6d72103cf
Kim In Seop <opnayn@gmail.com>
2018-09-25 19:17:04 +0900

    BaekJoon/1: Add 5.Dog
    Signed-off-by: Kim In Seop <opnayn@gmail.com>

---
+++ BaekJoon/1. IO/5.Dog.kt
@@ -0,0 +1,16 @@
+/*
+ * Dog
+ *
+ * https://www.acmicpc.net/problem/10172
+ */
+
+fun main(args: Array<String>) {
+    // 출력할때 일부 특수문자는 '\'(역슬래시)를 붙여 출력합니다.
+    // 탭, 개행문자, 따옴표 등이있으며, 여기서는 따옴표, 역슬래시를 사용했습니다.
+    // 이는 이스케이프 시퀀스(escape sequence)라 부릅니다.
+    println("|\\_/|")
+    println("|q p|   /}")
+    println("( 0 )\"\"\"\\")
+    println("|\"^\"`    |")
+    println("||_/=\\\\__|")
+}
